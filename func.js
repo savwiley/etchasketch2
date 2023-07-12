8cd37d43dc797e92a7b57a4825035aafca223c1b
Savannaha <savwiley@yahoo.com>
2023-07-11 20:37:22 -0500

    change event listener to dragging, change variables to let, add comment wishlist

--- func.js
+++ func.js
@@ -1,19 +1,33 @@
+/* VARIABLES */
+let numb = 36;
+let color = "#000";
+
 /* DOMS */
 const container = document.querySelector("#container");
 const menu = document.querySelector("#menu");
 
-/* VARIABLES */
-let numb = 16;
-let color = "#000";
+/* FUNCTIONS */
 
-/* FUNCTION */
+/* grid */
 for (let i = 0; i < (numb * numb); i++) {
   const square = document.createElement('div');
   square.setAttribute('class', 'square');
   square.style.width = `${100 / numb}%`;
   square.style.height = `auto`;
-  square.addEventListener('mouseenter', (e) => {
+  square.addEventListener('dragover', (e) => {
     e.target.style.background = color;
-  })
+  });
+  square.addEventListener('click', (e) => {
+    e.target.style.background = color;
+  });
   container.appendChild(square);
 }
+
+/** WISHLIST
+ * - Ability to change amount of squares
+ * - Ability to change the color
+ * - Ability to change to rainbow
+ * - Ability to change to rgba
+ * - Ability to erase
+ * - Ability to clear
+ */
