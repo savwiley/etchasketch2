d95e5825c2e746d8a400c85cea8fc1f2c490d26e
Savannaha <savwiley@yahoo.com>
2023-07-11 20:20:33 -0500

    add color variable, add menu dom

--- func.js
+++ func.js
@@ -1,15 +1,19 @@
 /* DOMS */
 const container = document.querySelector("#container");
+const menu = document.querySelector("#menu");
 
-const numb = 20;
+/* VARIABLES */
+let numb = 16;
+let color = "#000";
 
+/* FUNCTION */
 for (let i = 0; i < (numb * numb); i++) {
   const square = document.createElement('div');
   square.setAttribute('class', 'square');
   square.style.width = `${100 / numb}%`;
   square.style.height = `auto`;
   square.addEventListener('mouseenter', (e) => {
-    e.target.style.background = "#000";
+    e.target.style.background = color;
   })
   container.appendChild(square);
 }
