d8374cfba38b9055d18e54701addd8e1534a28b0
Savannaha <savwiley@yahoo.com>
2023-07-13 18:19:27 -0500

    add ability to change grid squares and color

--- func.js
+++ func.js
@@ -1,6 +1,6 @@
 /* VARIABLES */
-let numb = 36;
-let color = "#000";
+let numb = 16;
+let color = "#000000";
 
 /* DOMS */
 const container = document.querySelector("#container");
@@ -8,24 +8,65 @@ const menu = document.querySelector("#menu");
 
 /* FUNCTIONS */
 
-/* grid */
-for (let i = 0; i < (numb * numb); i++) {
-  const square = document.createElement('div');
-  square.setAttribute('class', 'square');
-  square.style.width = `${100 / numb}%`;
-  square.style.height = `auto`;
+/* change numb */
+const numberChange = document.createElement('div');
+numberChange.setAttribute('class', 'changer');
+
+const numbInput = document.createElement('input');
+numbInput.setAttribute('type', 'number');
+numbInput.value = numb;
+
+const numbButton = document.createElement('button');
+numbButton.textContent = "Reset!";
+
+numbButton.addEventListener('click', () => {
+  const squareX = Array.from(document.querySelectorAll(".square"));
+  squareX.forEach(e => e.remove());
+  numb = numbInput.value;
+  grid(numb);
+});
+
+menu.appendChild(numberChange);
+numberChange.appendChild(numbInput);
+numberChange.appendChild(numbButton);
+
+/* change color */
+
+const colorChange = document.createElement('div');
+
+const colorPicker = document.createElement('input');
+colorPicker.setAttribute('type', 'color');
+colorPicker.addEventListener('change', (e) => {
+  color = e.target.value;
+});
+
+menu.appendChild(colorChange);
+colorChange.appendChild(colorPicker);
+
+/* make grid */
+
+const squareEvents = (square) => {
   square.addEventListener('dragover', (e) => {
     e.target.style.background = color;
   });
   square.addEventListener('click', (e) => {
     e.target.style.background = color;
   });
-  container.appendChild(square);
-}
+};
+
+const grid = (numb) => {
+  for (let i = 0; i < (numb * numb); i++) {
+    const square = document.createElement('div');
+    square.setAttribute('class', 'square');
+    square.style.width = `${100 / numb}%`;
+    square.style.height = `auto`;
+    squareEvents(square);
+    container.appendChild(square);
+  }
+};
+grid(numb);
 
 /** WISHLIST
- * - Ability to change amount of squares
- * - Ability to change the color
  * - Ability to change to rainbow
  * - Ability to change to rgba
  * - Ability to erase
